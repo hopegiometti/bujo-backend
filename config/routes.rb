db8a9f5c10d1ba9bf7378aedba01ed3a5cd45d49
Hope <hopegiometti@gmail.com>
2020-02-05 14:00:00 -0500

    fixing deploy bugs

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   resources :journals
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "journals#index"
 end
